8a7fa339bdcff51204751ff7db9d2a0f201e9def
Martin Polednik <mpolednik@redhat.com>
2017-11-09 14:48:37 +0100

    don't forget oh-my-zsh
    Signed-off-by: Martin Polednik <mpolednik@redhat.com>

--- initzsh.sh
+++ initzsh.sh
@@ -1,1 +1,4 @@
 ln -s ~/Projects/src/github.com/mpolednik/dotfiles/.zshrc ~/.zshrc
+
+# requires ZSH though
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
