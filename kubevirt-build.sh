85bfb9707b0438ccbcaad873ef84ebc2bf98229b
Martin Polednik <mpolednik@redhat.com>
2017-11-29 12:08:33 +0100

    create vagrant-kubevirt dir before rsyncing into it
    Signed-off-by: Martin Polednik <mpolednik@redhat.com>

--- kubevirt-build.sh
+++ kubevirt-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-~/Projects/src/github.com/mpolednik/byoc/byoc kubevirt ~/Projects/src/kubevirt.io ~/kubevirt-vagrant/src
 mkdir -p ~/kubevirt-vagrant/src
+~/Projects/src/github.com/mpolednik/byoc/byoc kubevirt ~/Projects/src/kubevirt.io ~/kubevirt-vagrant/src
 pushd ~/kubevirt-vagrant/src/kubevirt.io/kubevirt
 VAGRANT_NUM_NODES=2 vagrant up
 sed "s/make all DOCKER_TAG=devel//g" cluster/vagrant/sync_build.sh | bash -
