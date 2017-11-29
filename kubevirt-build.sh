2d4517890c49597a24b46337304b39ae61c11254
Martin Polednik <mpolednik@redhat.com>
2017-11-29 13:55:04 +0100

    swap kubevirt-build and kubevirt-vagrant-deploy
    mistakes happen
    
    Signed-off-by: Martin Polednik <mpolednik@redhat.com>

--- kubevirt-build.sh
+++ kubevirt-build.sh
@@ -2,10 +2,5 @@
 
 source ./kubevirt-common.sh
 
-pushd ~/kubevirt-vagrant/src/kubevirt.io/kubevirt
-vagrant up
-sed "s/make all DOCKER_TAG=devel//g" cluster/vagrant/sync_build.sh | bash -
-./cluster/vagrant/sync_config.sh
-make manifests
-./cluster/deploy.sh
-popd
+mkdir -p ~/kubevirt-vagrant/src
+~/Projects/src/github.com/mpolednik/byoc/byoc kubevirt ~/Projects/src/kubevirt.io ~/kubevirt-vagrant/src
