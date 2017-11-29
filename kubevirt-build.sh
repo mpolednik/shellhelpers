185d0611493481c37b94288270936bddba521903
Martin Polednik <mpolednik@redhat.com>
2017-11-29 14:17:08 +0100

    kubevirt-build: pass args to byoc
    Signed-off-by: Martin Polednik <mpolednik@redhat.com>

--- kubevirt-build.sh
+++ kubevirt-build.sh
@@ -3,4 +3,4 @@
 source ./kubevirt-common.sh
 
 mkdir -p ~/kubevirt-vagrant/src
-~/Projects/src/github.com/mpolednik/byoc/byoc kubevirt ~/Projects/src/kubevirt.io ~/kubevirt-vagrant/src
+~/Projects/src/github.com/mpolednik/byoc/byoc kubevirt ~/Projects/src/kubevirt.io ~/kubevirt-vagrant/src $@
