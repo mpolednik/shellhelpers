aa4de8e33be8f74c117dae87344eaced0a96ba4c
Martin Polednik <mpolednik@redhat.com>
2017-11-29 13:53:56 +0100

    split kubevirt-build and kubevirt-vagrant-deploy
    Signed-off-by: Martin Polednik <mpolednik@redhat.com>

--- kubevirt-build.sh
+++ kubevirt-build.sh
@@ -2,8 +2,6 @@
 
 source ./kubevirt-common.sh
 
-mkdir -p ~/kubevirt-vagrant/src
-~/Projects/src/github.com/mpolednik/byoc/byoc kubevirt ~/Projects/src/kubevirt.io ~/kubevirt-vagrant/src
 pushd ~/kubevirt-vagrant/src/kubevirt.io/kubevirt
 vagrant up
 sed "s/make all DOCKER_TAG=devel//g" cluster/vagrant/sync_build.sh | bash -
---
+++ kubevirt-vagrant-deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source ./kubevirt-common.sh
+
+mkdir -p ~/kubevirt-vagrant/src
+~/Projects/src/github.com/mpolednik/byoc/byoc kubevirt ~/Projects/src/kubevirt.io ~/kubevirt-vagrant/src
