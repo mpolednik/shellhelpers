9dc2d2b6e8f9f16e854e3114e4cf25020b95ea14
root <root@localhost.localdomain>
2018-04-23 13:14:01 +0200

    fix etcdctl privkey
    Signed-off-by: root <root@localhost.localdomain>

--- etcdctl.sh
+++ etcdctl.sh
@@ -1,1 +1,1 @@
-ETCDCTL_API=3 ~/Projects/src/github.com/coreos/etcd/bin/etcdctl --cacert ~/Kube/certs/ca/ca.pem --key ~/Kube/certs/etcd/privkey.pem --cert ~/Kube/certs/etcd/etcd.pem --endpoints https://localhost:4001 $@
+ETCDCTL_API=3 ~/Projects/src/github.com/coreos/etcd/bin/etcdctl --cacert ~/Kube/certs/ca/ca.pem --key ~/Kube/certs/etcd/etcd.key --cert ~/Kube/certs/etcd/etcd.pem --endpoints https://localhost:4001 $@
